c61fa1a47911303c3ea0832d29d5c23748933987
Kyle Barron <kylebarron2@gmail.com>
2020-10-03 17:00:20 -0600

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -15,37 +15,6 @@
 
 namespace py = pybind11;
 
-int num_pixels(std::string inFile) {
-    const auto hm = std::make_shared<Heightmap>(inFile);
-    int w = hm->Width();
-    int h = hm->Height();
-    return w * h;
-}
-
-py::array_t<double> add_arrays(py::array_t<double> input1, py::array_t<double> input2) {
-    py::buffer_info buf1 = input1.request(), buf2 = input2.request();
-
-    if (buf1.ndim != 1 || buf2.ndim != 1)
-        throw std::runtime_error("Number of dimensions must be one");
-
-    if (buf1.size != buf2.size)
-        throw std::runtime_error("Input shapes must match");
-
-    /* No pointer is passed, so NumPy will allocate the buffer */
-    auto result = py::array_t<double>(buf1.size);
-
-    py::buffer_info buf3 = result.request();
-
-    double *ptr1 = (double *) buf1.ptr,
-           *ptr2 = (double *) buf2.ptr,
-           *ptr3 = (double *) buf3.ptr;
-
-    for (size_t idx = 0; idx < buf1.shape[0]; idx++)
-        ptr3[idx] = ptr1[idx] + ptr2[idx];
-
-    return result;
-}
-
 struct PydelatinTriangulator {
     PydelatinTriangulator(
       const std::vector<float> &data, const int width, const int height,
@@ -170,20 +139,6 @@ PYBIND11_MODULE(_pydelatin, m) {
         .def("getTriangles", &PydelatinTriangulator::getTriangles)
         .def("run", &PydelatinTriangulator::run)
         ;
-
-    m.def("num_pixels", &num_pixels, R"pbdoc(
-        Say Hello world!
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("add_arrays", &add_arrays, "Add two NumPy arrays");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
 
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
